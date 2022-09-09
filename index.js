015028f2f24559df2006f6889da6332dde6c40eb
llashishll <65934167+llashishll@users.noreply.github.com>
2022-09-09 11:08:44 +0530

    Added file from Local Machine

---
+++ index.js
@@ -0,0 +1,30 @@
+const buyingPrice = document.querySelector("#buying-price")
+const currentPrice = document.querySelector("#current-price")
+const subBtn = document.querySelector("#sub-btn")
+const outputBox = document.querySelector("#output")
+
+function calculateProfitLoss() {
+
+    let bp = Number(buyingPrice.value)
+    let cp = Number(currentPrice.value)
+
+    if (bp > cp) {
+
+        var loss = bp - cp;
+        var lossPercentage = loss / cp * 100
+
+        outputBox.innerText = 'Your loss is ' + loss + ' and lose percentage is ' + lossPercentage + '%'
+    } else if (bp < cp) {
+
+        var profit = cp - bp
+        var profitPercentage = profit / cp * 100
+
+        outputBox.innerText = 'Your profit is ' + profit + ' and lose percentage is ' + profitPercentage + '%'
+
+
+    } else {
+        outputBox.innerText = "There is neither loss nor profit"
+    }
+
+}
+subBtn.addEventListener("click", calculateProfitLoss)
