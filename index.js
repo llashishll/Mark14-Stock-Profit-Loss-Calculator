0f13a52d4414818b35c73c255a0890acc552b921
llashishll <ashishkalra999@gmail.com>
2022-09-10 10:34:28 +0530

    fix: 1. program was not considering the purchased quantity .      2. Formula of Profit/Loss Percentage was incorrect.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const buyingPrice = document.querySelector("#buying-price")
 const currentPrice = document.querySelector("#current-price")
+const qty = document.querySelector("#quantity")
 const subBtn = document.querySelector("#sub-btn")
 const outputBox = document.querySelector("#output")
 
 function calculateProfitLoss() {
 
-    let bp = Number(buyingPrice.value)
-    let cp = Number(currentPrice.value)
+    let bp = Number(buyingPrice.value) * Number(qty.value);
+    let cp = Number(currentPrice.value) * Number(qty.value);
 
     if (bp > cp) {
 
-        var loss = bp - cp;
-        var lossPercentage = loss / cp * 100
+        var loss = (bp - cp)
+        var lossPercentage = (loss / bp) * 100
 
         outputBox.innerText = 'Your loss is ' + loss + ' and lose percentage is ' + lossPercentage + '%'
     } else if (bp < cp) {
 
-        var profit = cp - bp
-        var profitPercentage = profit / cp * 100
+        var profit = (cp - bp)
+        var profitPercentage = (profit / bp) * 100
 
         outputBox.innerText = 'Your profit is ' + profit + ' and profit percentage is ' + profitPercentage + '%'
 
